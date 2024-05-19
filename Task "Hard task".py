d6db044c01f023902836df74aa85c86bf9c358c1
Tomas26071988 <artem_1988_artem1988@icloud.com>
2024-05-19 21:42:57 +0300

    Add files via upload

---
+++ Task "Hard task".py
@@ -0,0 +1,9 @@
+print('Введите количество (кг)')
+kg = float(input(4))
+print('Введите цену')
+price = float(input(34))
+print("Введите сумму платежа")
+summa = float(input(1000))
+ch = (summa - (price * kg))
+print("Сумма сдачи", ch)
+#Задача "сдача всем"
